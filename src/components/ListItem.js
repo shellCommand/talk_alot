0ac45a1f6be118f8f9a13b7245e4add57144d555
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-21 15:07:35 -0500

    add some style

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,20 @@
+import TickItem from './TickItem'
+import ReadMessage from './ReadMessage'
+
 const ListItem = ({message}) => {
   return (
-    <div className="List-Item">
-      <p>{message.user_email}:  {message.title}</p>
-    </div>
+    <li className="List-Item">
+      <div className='message-container'>
+        <TickItem/>
+        <p className='messages'><span className='user-name'>{message.user_email}</span>:  {message.title}</p>
+        <ReadMessage/>
+        <div className='button-container'>
+          <button className='edit'>EDIT</button>
+          <button className='de;ete'>DELETE</button>
+        </div>
+      </div>
+
+    </li>
   )
 }
 
