bfae6ba5c077e0c148d93b00a828d38258b2a237
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-21 12:37:03 -0500

    pulling data from backend to front -- added cors in server.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import ListHeader from './components/ListHeader'
+import { useEffect } from 'react'
 
 const App = () => {
+
+  const getData = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/messages`)
+      // const res = await fetch(`http://localhost:8000/messages${userEmail}`)
+      const json = await res.json()
+      console.log(json)
+    } catch (err) {
+        console.error(err)
+    }
+  }
+
+  useEffect(() => getData, [])
+
   return (
     <div className="App">
       <ListHeader listName={'🥸 TalkAlot'} />
