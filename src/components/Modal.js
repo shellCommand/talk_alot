d61f595f7bcd0a2a70d985ecd481ea6247577524
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-21 19:51:09 -0500

    allow for edits in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,33 @@
+import { useState } from 'react'
+
 const Modal = () => {
+  const editMode = 'create'
+  const [data, setData] = useState({
+    user_email: '',
+    title: '',
+    // date: new Date()
+    date: editMode ? '' : new Date()
+  })
+
   let mode = 'Create'
-  const handleChange = () => {
-    console.log('changing')
+  const handleChange = (e) => {
+    const {name, value } = e.target
+    setData(data => ({
+      ...data,
+      [name] : value
+    }))
+    console.log(data)
   }
 
   return (
     <div className='overlay'>
       <div className='modal'>
         <div className='form-title-container'>
-          <h3>{mode} new message</h3>
+          <h3>{mode} message</h3>
           <button>X</button>
         </div>
           <form>
-            <input
+            <input className='message-input'
               required
               maxLength={5000}
               placeholder='Type away...'
