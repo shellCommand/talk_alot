c83e42e37c7e2b7a5d89c795812d7aa3d066c1e2
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-23 07:23:51 -0500

    successful delete functionality

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,8 +2,22 @@ import { useState } from 'react'
 import TickIcon from './TickIcon'
 import Modal from './Modal'
 
-const ListItem = ({message}) => {
+const ListItem = ({message, getData}) => {
 const [showModal, setShowModal] = useState(false)
+
+  const deleteItem = async() => {
+    try {
+      const response = await fetch(`http://localhost:8000/messages/${message.id}`, {
+        method: 'DELETE'
+      })
+      if (response.status === 200) {
+        getData()
+      }
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
   return (
     <li className="List-Item">
       <div className='message-container'>
@@ -12,9 +26,9 @@ const [showModal, setShowModal] = useState(false)
         <p className='messages'><span className='user-name'>{message.user_email}</span>:  {message.title}</p>
         <div className='button-container'>
           <button className='edit' onClick={() => setShowModal(true)}>EDIT</button>
-          <button className='delete'>DELETE</button>
+          <button className='delete' onClick={deleteItem}>DELETE</button>
         </div>
-        {showModal && <Modal mode={'Edit'} setShowModal={setShowModal} message={message} />}
+        {showModal && <Modal mode={'Edit'} setShowModal={setShowModal} message={message} getData={getData} />}
       </div>
 
     </li>
