c7223597ac13d666b8dd190c9a59dc102bb10767
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-23 13:12:21 -0500

    add auth component

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,34 @@
-const Auth = () => {
-  return (
-    <div className="Auth">
+import { useState } from 'react'
 
+const Auth = () => {
+  const [isLogIn, setIsLogin] = useState(true)
+  const [error, setError] = useState(null)
+  const isLogin = true
+
+  const viewLogin = (status) => {
+    setError(null)
+    setIsLogin(status)
+  }
+  return (
+    <div className='auth-container'>
+      <div className='auth-container-box'>
+        <form>
+          <h2>{isLogin ? 'Please log in' : 'Please sign up'}</h2>
+          <input type='email' placeholder='Email'/>
+          <input type='password' placeholder='Password'/>
+          {!isLogin && <input type='password' placeholder='Confirm password'/>}
+          <input type='submit' className='create'/>
+          {error && <p>{error}</p>}
+        </form>
+        <div className='auth-options'>
+          <button onClick={() => viewLogin(false)}
+          style={{backgroundColor : !isLogin ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)'}}
+          >Sign up</button>
+          <button onClick={() => viewLogin(true)}
+          style={{backgroundColor : isLogin ? 'rgb(255, 255, 255)' : 'rgb(188, 188, 188)'}}
+          >Login</button>
+        </div>
+      </div>
     </div>
   )
 }
