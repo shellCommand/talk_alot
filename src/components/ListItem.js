c218f6d132db2daf7acc4c894998b66f73d1e7a1
commandNotFound <tuerkbot@talkingRobot.local>
2023-05-23 06:52:20 -0500

    style date, move buttons

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,8 +7,8 @@ const [showModal, setShowModal] = useState(false)
   return (
     <li className="List-Item">
       <div className='message-container'>
-      <TickIcon/>
       <p className='date'>{message.date.split('T')[0]}</p>
+      <TickIcon/>
         <p className='messages'><span className='user-name'>{message.user_email}</span>:  {message.title}</p>
         <div className='button-container'>
           <button className='edit' onClick={() => setShowModal(true)}>EDIT</button>
